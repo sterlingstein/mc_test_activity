60219a695eab97c13a135197191e765e8819d299
Sterling Stein <r632791@L261612.regence.com>
2019-11-07 12:54:13 -0800

    we need to ready through postmonger before doing anything else, so the doc ready doesn't make sense here.

--- simpleActivity.js
+++ simpleActivity.js
@@ -1,20 +1,13 @@
 const connection = new Postmonger.Session();
-$(window).ready(onRender);
-
-function onRender() {
-    console.log("called onRender");
-    // JB will respond the first time 'ready' is called with 'initActivity'
-    connection.trigger('ready');
-    connection.trigger('requestTokens');
-    connection.trigger('requestEndpoints');
-    connection.on('initActivity', data => {
-      console.log("called initActivity");
-      document.getElementById('configuration').value = JSON.stringify(data, null, 2);
-    });
-    connection.on('clickedNext', () => {
-      console.log("called clickedNext");
-      const configuration = JSON.parse(document.getElementById('configuratin').value);
-      // UpdateActivity saves our configuration into Journey Builder.
-      connection.trigger('updateActivity', configuration);
-    });
-}
+// JB will respond the first time 'ready' is called with 'initActivity'
+connection.trigger('ready');
+connection.on('initActivity', data => {
+  console.log("called initActivity");
+  document.getElementById('configuration').value = JSON.stringify(data, null, 2);
+});
+connection.on('clickedNext', () => {
+  console.log("called clickedNext");
+  const configuration = JSON.parse(document.getElementById('configuratin').value);
+  // UpdateActivity saves our configuration into Journey Builder.
+  connection.trigger('updateActivity', configuration);
+});
